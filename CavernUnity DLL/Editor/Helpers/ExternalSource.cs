981ca04d601c6367bfcb8116f4cd5646d7bd59d4
VoidX <voidxb@gmail.com>
2017-10-09 01:01:34 +0200

    Audio input handler

---
+++ CavernUnity DLL/Editor/Helpers/ExternalSource.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Cavern.Helpers {
+    [CustomEditor(typeof(ExternalSource))]
+    public class ExternalSourceInspector : Editor {
+        float LastLatency;
+
+        public override void OnInspectorGUI() {
+            DrawDefaultInspector();
+            if (Application.isPlaying)
+                GUILayout.Label("Latency: " + ((ExternalSource)target).Latency);
+        }
+
+        void OnSceneGUI() {
+            float Latency = ((ExternalSource)target).Latency;
+            if (LastLatency != Latency) {
+                LastLatency = Latency;
+                Repaint();
+            }
+        }
+    }
+}
---
+++ CavernUnity DLL/Helpers/ExternalSource.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Cavern.Helpers {
+    /// <summary>Handles and synchronizes an <see cref="AudioSource3D"/> with an input device.</summary>
+    [AddComponentMenu("Audio/Helpers/External Source")]
+    public class ExternalSource : MonoBehaviour {
+        /// <summary>Target source.</summary>
+        [Tooltip("Target source.")]
+        public AudioSource3D Source;
+        /// <summary>Audio input device.</summary>
+        [Tooltip("Audio input device.")]
+        public string SourceName;
+        /// <summary>Maximum allowed latency.</summary>
+        [Tooltip("Maximum allowed latency.")]
+        [Range(.02f, .5f)] public float MaxLatency = .05f;
+
+        /// <summary>Actual latency.</summary>
+        internal float Latency { get; private set; }
+
+        void OnEnable() {
+            int MinFreq, MaxFreq;
+            Source.Loop = true;
+            Microphone.GetDeviceCaps(SourceName, out MinFreq, out MaxFreq);
+            Source.clip = Microphone.Start(SourceName, true, 1, MaxFreq);
+            Source.clip.name = SourceName;
+            Source.Play();
+        }
+
+        void Update() {
+            if (!Source)
+                return;
+            // Latency fix
+            int MicPos = Microphone.GetPosition(Source.clip.name);
+            Latency = (MicPos - Source.timeSamples + Source.clip.samples) % Source.clip.samples / (float)Source.clip.samples;
+            if (Latency > MaxLatency)
+                Source.timeSamples = MicPos - AudioSettings.GetConfiguration().dspBufferSize;
+        }
+
+        void OnDisable() {
+            Destroy(Source.clip);
+        }
+    }
+}
