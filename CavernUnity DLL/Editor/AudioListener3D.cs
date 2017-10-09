e03e0571ff0c92cb27c080e6452b1bdbf6f69d4b
VoidX <voidxb@gmail.com>
2017-10-09 02:15:06 +0200

    Don't compile editor scripts for standalone DLLs

--- CavernUnity DLL/Editor/AudioListener3D.cs
+++ CavernUnity DLL/Editor/AudioListener3D.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿#if UNITY_EDITOR
+using UnityEditor;
 using UnityEngine;
 
 namespace Cavern {
@@ -24,3 +25,4 @@ namespace Cavern {
         }
     }
 }
+#endif
--- CavernUnity DLL/Editor/Helpers/ExternalSource.cs
+++ CavernUnity DLL/Editor/Helpers/ExternalSource.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿#if UNITY_EDITOR
+using UnityEditor;
 using UnityEngine;
 
 namespace Cavern.Helpers {
@@ -15,3 +16,4 @@ namespace Cavern.Helpers {
         }
     }
 }
+#endif
