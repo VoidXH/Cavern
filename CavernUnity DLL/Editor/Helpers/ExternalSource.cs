bcb373ce3b8bb461d1ad26a3b392c374d61cad65
VoidX <voidxb@gmail.com>
2017-10-11 01:05:24 +0200

    Extra debug information for external sources

--- CavernUnity DLL/Editor/Helpers/ExternalSource.cs
+++ CavernUnity DLL/Editor/Helpers/ExternalSource.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 namespace Cavern.Helpers {
     [CustomEditor(typeof(ExternalSource))]
     public class ExternalSourceInspector : Editor {
+        float LastDelay = 0, LastLatency = 0;
+
         public override void OnInspectorGUI() {
+            float LerpValue = 20 * Time.deltaTime;
+
             DrawDefaultInspector();
-            if (Application.isPlaying)
-                GUILayout.Label("Latency: " + ((ExternalSource)target).Latency);
+            if (Application.isPlaying) {
+                GUILayout.Space(16);
+                GUILayout.Label("Listener delay: " +
+                    (LastDelay = CavernUtilities.FastLerp(LastDelay, (float)AudioListener3D.BufferPosition /
+                    (AudioListener3D.ChannelCount * AudioListener3D.Current.SampleRate), LerpValue)) + " s");
+                GUILayout.Label("Input latency: " +
+                    (LastLatency = CavernUtilities.FastLerp(LastLatency, ((ExternalSource)target).Latency, LerpValue)) + " s");
+                GUILayout.Label("Actual latency: " + (LastDelay + LastLatency) + " s");
+            }
         }
 
         public override bool RequiresConstantRepaint() {
