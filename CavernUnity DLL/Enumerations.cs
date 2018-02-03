eec55fac9a83e310e3e19d266e349cb36d7c77cf
VoidX <voidxb@gmail.com>
2018-02-03 17:25:13 +0100

    Updated quality mode summaries

--- CavernUnity DLL/Enumerations.cs
+++ CavernUnity DLL/Enumerations.cs
@@ -19,7 +19,7 @@
         /// Doppler effect and pitch shifting: No.
         /// Resampling quality: Low.
         /// Channels mixed to 3D space: First.
-        /// Fading: Constant gain.
+        /// Panning: Constant gain.
         /// Accurate angle calculation in asymmetric mode: No.
         /// Non-approximated 3D mix for Studio/Home in asymmetric mode: No.
         /// </summary>
@@ -30,7 +30,7 @@
         /// Doppler effect and pitch shifting: Low quality.
         /// Resampling quality: Low.
         /// Channels mixed to 3D space: First.
-        /// Fading: Constant gain.
+        /// Panning: Constant gain.
         /// Accurate angle calculation in asymmetric mode: No.
         /// Non-approximated 3D mix for Studio/Home in asymmetric mode: No.
         /// </summary>
@@ -41,7 +41,7 @@
         /// Doppler effect and pitch shifting: High quality.
         /// Resampling quality: High.
         /// Channels mixed to 3D space: All.
-        /// Fading: Constant power.
+        /// Panning: Approximated constant power.
         /// Accurate angle calculation in asymmetric mode: Yes.
         /// Non-approximated 3D mix for Studio/Home in asymmetric mode: No.
         /// </summary>
@@ -52,7 +52,7 @@
         /// Doppler effect and pitch shifting: High quality.
         /// Resampling quality: High.
         /// Channels mixed to 3D space: All.
-        /// Fading: Constant power.
+        /// Panning: Constant power.
         /// Accurate angle calculation in asymmetric mode: Yes.
         /// Non-approximated 3D mix for Studio/Home in asymmetric mode: Yes.
         /// </summary>
