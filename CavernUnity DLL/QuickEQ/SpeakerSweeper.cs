7ca9c77f7d607fd6f287706dc13526f90ee5d949
VoidX <voidxb@gmail.com>
2018-08-02 22:59:12 +0200

    Environment dependent sweeper object positioning

--- CavernUnity DLL/QuickEQ/SpeakerSweeper.cs
+++ CavernUnity DLL/QuickEQ/SpeakerSweeper.cs
@@ -87,7 +87,10 @@ namespace Cavern.QuickEQ {
 
         void MeasurementStart() {
             GameObject SweeperObj = new GameObject();
-            SweeperObj.transform.position = CavernUtilities.VectorScale(AudioListener3D.Channels[Channel].SphericalPos, AudioListener3D.EnvironmentSize * 2);
+            if (AudioListener3D._EnvironmentType != Environments.Studio)
+                SweeperObj.transform.position = CavernUtilities.VectorScale(AudioListener3D.Channels[Channel].CubicalPos, AudioListener3D.EnvironmentSize);
+            else
+                SweeperObj.transform.position = CavernUtilities.VectorScale(AudioListener3D.Channels[Channel].SphericalPos, AudioListener3D.EnvironmentSize);
             Sweeper = SweeperObj.AddComponent<AudioSource3D>();
             Sweeper.Clip = Sweep;
             Sweeper.IsPlaying = true;
