4cdfb5f29ec8de86631ac69fda1360ee7131c4fc
VoidX <voidxb@gmail.com>
2018-09-01 02:33:30 +0200

    More verbose impulse response

--- CavernUnity DLL/QuickEQ/Utils/VerboseImpulseResponse.cs
+++ CavernUnity DLL/QuickEQ/Utils/VerboseImpulseResponse.cs
@@ -3,6 +3,8 @@
 namespace Cavern.QuickEQ {
     /// <summary>Contains an impulse response and data that can be calculated from it.</summary>
     public class VerboseImpulseResponse {
+        /// <summary>Raw impulse response on the complex plane.</summary>
+        public Complex[] ComplexResponse;
         /// <summary>Raw impulse response samples.</summary>
         public float[] Response;
         /// <summary>Impulse polarity, true if positive.</summary>
@@ -12,7 +14,7 @@ namespace Cavern.QuickEQ {
 
         /// <summary>Create a verbose impulse response from a precalculated impulse response.</summary>
         public VerboseImpulseResponse(Complex[] ImpulseResponse) {
-            Response = Measurements.GetRealPart(ImpulseResponse);
+            Response = Measurements.GetRealPart(ComplexResponse = ImpulseResponse);
             float AbsPeak = float.NegativeInfinity;
             for (int Pos = 0, Length = Response.Length; Pos < Length; ++Pos) {
                 float AbsHere = Math.Abs(Response[Pos]);
@@ -26,6 +28,6 @@ namespace Cavern.QuickEQ {
 
         /// <summary>Create a verbose impulse response from a reference signal and a recorded response.</summary>
         public VerboseImpulseResponse(float[] Reference, float[] Response) :
-            this(Measurements.GetFrequencyResponse(Reference, Response)) { }
+            this(Measurements.IFFT(Measurements.GetFrequencyResponse(Reference, Response))) { }
     }
 }
