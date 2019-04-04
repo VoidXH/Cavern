ca882bcc80bd19d218ceefc14f6f9281a1b0ea2f
VoidX <voidxb@gmail.com>
2019-04-04 20:20:37 +0200

    Simple echo filter interface

---
+++ CavernUnity DLL/FilterInterfaces/SimpleEcho.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+using Cavern.Filters;
+
+namespace Cavern.FilterInterfaces {
+    /// <summary>Apply an <see cref="Echo"/> filter on the source the component is applied on.</summary>
+    [AddComponentMenu("Audio/Filters/Simple Echo")]
+    [RequireComponent(typeof(AudioSource3D))]
+    public class SimpleEcho : MonoBehaviour {
+        /// <summary>Effect strength.</summary>
+        [Tooltip("Effect strength.")]
+        [Range(0, 1)] public float Strength;
+        /// <summary>Delay between echoes in seconds.</summary>
+        [Tooltip("Delay between echoes in seconds.")]
+        [Range(0.01f, 1f)] public float Delay;
+
+        /// <summary>The attached audio source.</summary>
+        AudioSource3D Source;
+        /// <summary>The attached echo filter.</summary>
+        Echo Filter;
+
+        void Start() {
+            Source = GetComponent<AudioSource3D>();
+            Filter = new Echo(Strength, Delay);
+            Source.AddFilter(Filter);
+        }
+
+        void OnDestroy() => Source.RemoveFilter(Filter);
+
+        void Update() {
+            int TargetDelay = (int)(Delay * AudioListener3D.Current.SampleRate);
+            if (Filter.DelaySamples != TargetDelay)
+                Filter.DelaySamples = TargetDelay;
+            Filter.Strength = Strength;
+        }
+    }
+}
