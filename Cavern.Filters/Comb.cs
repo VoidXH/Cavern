d8fe7e0c4f0071ccfbdcff16c0484c60d659ffe0
VoidX <voidxb@gmail.com>
2019-06-04 19:48:32 +0200

    Comb filter

---
+++ Cavern.Filters/Comb.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Cavern.Filters {
+    /// <summary>Normalized feedforward comb filter.</summary>
+    /// <remarks>The feedback comb filter is called <see cref="Echo"/>.</remarks>
+    public class Comb : Filter {
+        /// <summary>Delay in samples.</summary>
+        public int K {
+            get => delay.DelaySamples;
+            set => delay.DelaySamples = value;
+        }
+
+        /// <summary>First minimum point.</summary>
+        public float Frequency {
+            get => sampleRate * .5f / K;
+            set => K = (int)(.5f / (value / sampleRate) + 1);
+        }
+
+        /// <summary>Wet mix multiplier.</summary>
+        public float Alpha;
+
+        /// <summary>Delay filter generating the samples fed forward.</summary>
+        readonly Delay delay;
+        /// <summary>Array used to hold samples processed by <see cref="delay"/>.</summary>
+        float[] cache = new float[0];
+        /// <summary>Cached source sample rate.</summary>
+        readonly int sampleRate;
+
+        /// <summary>Normalized feedforward comb filter.</summary>
+        /// <param name="sampleRate">Source sample rate</param>
+        /// <param name="K">Delay in samples</param>
+        /// <param name="alpha">Wet mix multiplier</param>
+        public Comb(int sampleRate, int K, float alpha) {
+            this.sampleRate = sampleRate;
+            Alpha = alpha;
+            delay = new Delay(K);
+        }
+
+        /// <summary>Normalized feedforward comb filter.</summary>
+        /// <param name="sampleRate">Source sample rate</param>
+        /// <param name="frequency">First minimum point</param>
+        /// <param name="alpha">Wet mix multiplier</param>
+        public Comb(int sampleRate, float frequency, float alpha) {
+            this.sampleRate = sampleRate;
+            Alpha = alpha;
+            delay = new Delay((int)(.5f / (frequency / sampleRate) + 1));
+        }
+
+        /// <summary>Apply comb on an array of samples. One filter should be applied to only one continuous stream of samples.</summary>
+        public override void Process(float[] samples) {
+            int sampleCount = samples.Length;
+            if (cache.Length != sampleCount)
+                cache = new float[sampleCount];
+            Array.Copy(samples, cache, sampleCount);
+            delay.Process(cache);
+            float divisor = 1 / (1 + Alpha);
+            for (int sample = 0; sample < sampleCount; ++sample)
+                samples[sample] = (samples[sample] + cache[sample] * Alpha) * divisor;
+        }
+    }
+}
---
+++ CavernUnity DLL/FilterInterfaces/Comb.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Cavern.FilterInterfaces {
+    /// <summary>Apply a <see cref="Filters.Comb"/> filter on the source this component is applied on.</summary>
+    [AddComponentMenu("Audio/Filters/Comb")]
+    [RequireComponent(typeof(AudioSource3D))]
+    public class Comb : MonoBehaviour {
+        /// <summary>First minimum point.</summary>
+        [Tooltip("First minimum point.")]
+        [Range(20, 3000)] public float Frequency = 1000;
+        /// <summary>Wet mix multiplier.</summary>
+        [Tooltip("Wet mix multiplier")]
+        [Range(0, 3)] public float Alpha;
+
+        /// <summary>The attached audio source.</summary>
+        AudioSource3D Source;
+        /// <summary>The attached delay filter.</summary>
+        Filters.Comb Filter;
+
+        void OnEnable() {
+            Source = GetComponent<AudioSource3D>();
+            Filter = new Filters.Comb(AudioListener3D.Current.SampleRate, Frequency, Alpha);
+            Source.AddFilter(Filter);
+        }
+
+        void OnDisable() => Source.RemoveFilter(Filter);
+
+        void Update() {
+            Filter.Frequency = Frequency;
+            Filter.Alpha = Alpha;
+        }
+    }
+}
