6eaf1a5c8f6b5da09881092096efe90ed58aec5d
VoidX <voidxb@gmail.com>
2020-11-13 15:17:29 +0100

    Q-factor from slope

--- Cavern.Filters/Utilities/QFactor.cs
+++ Cavern.Filters/Utilities/QFactor.cs
@@ -17,5 +17,21 @@ namespace Cavern.Filters.Utilities {
             double pow = Math.Pow(2, octaves);
             return Math.Sqrt(pow) / (pow - 1);
         }
+
+        /// <summary>Convert slope to Q-factor.</summary>
+        /// <param name="slope">Filter steepness factor</param>
+        /// <param name="gain">Filter gain in decibels</param>
+        public static double FromSlope(double slope, double gain) {
+            double a = Math.Pow(10, gain * .05f);
+            return 1.0 / Math.Sqrt((a + 1 / a) * (1 / slope - 1) + 2);
+        }
+
+        /// <summary>Convert slope to Q-factor.</summary>
+        /// <param name="slope">Filter steepness in decibels</param>
+        /// <param name="gain">Filter gain in decibels</param>
+        public static double FromSlopeDecibels(double slope, double gain) {
+            double a = Math.Pow(10, gain * .05f);
+            return 1.0 / Math.Sqrt((a + 1 / a) * (1 / Math.Abs(slope / gain) - 1) + 2);
+        }
     }
 }
