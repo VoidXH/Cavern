ec7d14c9197a6fda336f30d5283d2c2b84a2831f
Bence Sgánetz <voidxb@gmail.com>
2021-04-29 09:44:19 +0200

    CavernAmp bypass

--- Cavern/Utilities/CavernAmp.cs
+++ Cavern/Utilities/CavernAmp.cs
@@ -1,20 +1,23 @@
 ﻿using System.Runtime.InteropServices;
 
 namespace Cavern.Utilities {
-    /// <summary>
-    /// Drastically faster versions of some functions written in C++.
-    /// </summary>
+    /// <summary>Drastically faster versions of some functions written in C++.</summary>
     public static class CavernAmp {
+        /// <summary>Is the CavernAmp DLL present and the platform is correct?</summary>
         static bool available;
+        /// <summary>Force disable CavernAmp for performance benchmarks.</summary>
+        static bool bypass = false;
+        /// <summary>True if CavernAmp DLL was checked if <see cref="available"/>.</summary>
         static bool tested = false;
 
-        /// <summary>
-        /// Is the CavernAmp DLL present and the platform is correct?
-        /// </summary>
+        /// <summary>Is the CavernAmp DLL present and the platform is correct?</summary>
         public static bool Available {
             get {
                 if (tested)
                     return available;
+
+                if (bypass)
+                    return available = false;
                 try {
                     available = IsAvailable();
                 } catch {
@@ -25,6 +28,18 @@ namespace Cavern.Utilities {
             }
         }
 
+        /// <summary>Force disable CavernAmp for performance benchmarks.</summary>
+        public static bool Bypass {
+            set {
+                bypass = value;
+                if (available)
+                    available = false;
+                if (!bypass)
+                    tested = false;
+            }
+        }
+
+        /// <summary>When the DLL is present near the executable and the platform matches, this returns true.</summary>
         [DllImport("CavernAmp.dll", EntryPoint = "IsAvailable")]
         static extern bool IsAvailable();
     }
