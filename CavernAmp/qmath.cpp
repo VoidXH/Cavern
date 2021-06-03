be262f2dec18370de2a5e8dcfd5f6f990ed2dd9f
Bence Sgánetz <voidxb@gmail.com>
2021-06-03 02:43:12 +0200

    SumAbs in CavernAmp

--- CavernAmp/qmath.cpp
+++ CavernAmp/qmath.cpp
@@ -1,1 +1,10 @@
+#include <math.h>
+
 #include "qmath.h"
+
+float SumAbs(float* array, int arrayLength) {
+    float sum = 0;
+    for (int i = 0; i < arrayLength; ++i)
+        sum += fabsf(array[i]);
+    return sum;
+}
--- CavernAmp/qmath.h
+++ CavernAmp/qmath.h
@@ -1,6 +1,7 @@
 #ifndef QMATH_H
 #define QMATH_H
 
+// Compute the base 2 logarithm of a number faster than a generic Log function.
 inline int log2(const int x) {
   int y;
   asm ("\tbsr %1, %0\n"
@@ -8,5 +9,8 @@ inline int log2(const int x) {
       : "r" (x));
   return y;
 }
+
+// Sum absolute values of elements in an array.
+float SumAbs(float* array, int arrayLength);
 
 #endif // QMATH_H
