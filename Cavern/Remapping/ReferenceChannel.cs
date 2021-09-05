8e5beeeaa364223aebaf801e5cd62529714f18c7
Bence Sgánetz <voidxb@gmail.com>
2021-09-06 01:18:27 +0200

    First reference channels in 7.1 order for hacks

--- Cavern/Remapping/ReferenceChannel.cs
+++ Cavern/Remapping/ReferenceChannel.cs
@@ -1,15 +1,16 @@
 ﻿namespace Cavern.Remapping {
     /// <summary>Possible channels in channel-based legacy systems.</summary>
+    /// <remarks>The standard 7.1 layout is the first 8 entries.</remarks>
     public enum ReferenceChannel {
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
         FrontLeft,
         FrontRight,
         FrontCenter,
         ScreenLFE,
-        SideLeft,
-        SideRight,
         RearLeft,
         RearRight,
+        SideLeft,
+        SideRight,
         FrontLeftCenter,
         FrontRightCenter,
         HearingImpaired,
