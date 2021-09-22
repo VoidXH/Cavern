d6dd64e697d7eb4cb7ded3c88de8f72ce6af9432
Bence Sgánetz <voidxb@gmail.com>
2021-09-22 21:18:34 +0200

    Linked attribute

---
+++ Cavern/Utilities/Linked.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace Cavern.Utilities {
+    /// <summary>Marks a relationship between two arrays that are not merged in a struct for better performance.</summary>
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
+    public class Linked : ValidationAttribute {
+        /// <summary>The other field linked with this field.</summary>
+        readonly string targetField;
+
+        /// <summary>Marks a relationship between two arrays that are not merged in a struct for better performance.</summary>
+        public Linked(string targetField) => this.targetField = targetField;
+
+        /// <summary>Checks if a linking is valid in an object.</summary>
+        public override bool IsValid(object value) {
+            FieldInfo[] fields = value.GetType().GetFields(BindingFlags.Public | BindingFlags.NonPublic);
+            for (int field = 0; field < fields.Length; ++field)
+                if (fields[field].Name.Equals(targetField))
+                    return true;
+            return false;
+        }
+    }
+}
