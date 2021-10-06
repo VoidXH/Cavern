923fb74933b6541840d1b2f29cf2c9d750773aab
Bence Sgánetz <voidxb@gmail.com>
2021-10-07 00:43:39 +0200

    Faster convolution for spike-based impulses

---
+++ Cavern.Filters/SpikeConvolver.cs
@@ -0,0 +1,19 @@
+﻿namespace Cavern.Filters {
+    /// <summary>
+    /// Simple convolution window
+    /// </summary>
+    public class SpikeConvolver : Convolver {
+        /// <summary>Construct a spike convolver for a target impulse response.</summary>
+        public SpikeConvolver(float[] impulse, int delay) : base(impulse, delay) {}
+
+        /// <summary>Apply convolution on an array of samples. One filter should be applied to only one continuous stream of samples.</summary>
+        public override void Process(float[] samples) {
+            float[] convolved = new float[samples.Length + impulse.Length + delay];
+            for (int step = 0; step < impulse.Length; ++step)
+                if (impulse[step] != 0)
+                    for (int sample = 0; sample < samples.Length; ++sample)
+                        convolved[sample + step + delay] += samples[sample] * impulse[step];
+            Finalize(samples, convolved);
+        }
+    }
+}
