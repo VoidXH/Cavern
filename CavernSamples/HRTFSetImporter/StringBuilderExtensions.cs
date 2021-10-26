e05d38448c02c5fa5d57fa981f93c82ffb9ffb5d
Bence Sgánetz <voidxb@gmail.com>
2021-10-26 19:38:32 +0200

    String compression for array generation

--- CavernSamples/HRTFSetImporter/StringBuilderExtensions.cs
+++ CavernSamples/HRTFSetImporter/StringBuilderExtensions.cs
@@ -9,8 +9,15 @@ namespace HRTFSetImporter {
 
         public static StringBuilder AppendArray(this StringBuilder builder, float[] array) {
             builder.Append("new float[").Append(array.Length).Append("] { ");
-            for (int i = 0; i < array.Length; ++i)
-                builder.Append(array[i].ToString(numberFormat)).Append("f, ");
+            for (int i = 0; i < array.Length; ++i) {
+                string str = array[i].ToString(numberFormat);
+                if (str.StartsWith("0."))
+                    builder.Append(str.Substring(1)).Append("f, ");
+                else if (str.Length == 1)
+                    builder.Append("0, ");
+                else
+                    builder.Append(str).Append("f, ");
+            }
             builder.Remove(builder.Length - 2, 2).AppendLine(" },");
             return builder;
         }
