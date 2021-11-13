f96d4a88b467e1623bfcd1ac068aa02855a64d4f
Bence Sgánetz <voidxb@gmail.com>
2021-11-13 14:27:12 +0100

    Move timed test tone to QuickEQ

---
+++ Cavern.QuickEQ/SignalGeneration/TimedTestTone.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Cavern.QuickEQ.SignalGeneration {
+    /// <summary>Plays a test tone on a single channel, with a delay to let channels with a smaller ID play first.
+    /// An instance has to be created for all channels for complete system measurement.</summary>
+    public class TimedTestTone : Source {
+        /// <summary>Pregenerated test tone samples.</summary>
+        readonly float[] testTone;
+        /// <summary>Target output channel.</summary>
+        readonly int channel = 0;
+
+        /// <summary>Rendered output array kept to save allocation time.</summary>
+        float[] rendered = new float[0];
+
+        /// <summary>Create the source from any waveform.</summary>
+        public TimedTestTone(int channel, float[] testTone) {
+            this.channel = channel;
+            this.testTone = testTone;
+        }
+
+        /// <summary>Creates a cache and always marks this source for playback.</summary>
+        protected override bool Precollect() {
+            if (rendered.Length != Listener.Channels.Length * listener.UpdateRate)
+                rendered = new float[Listener.Channels.Length * listener.UpdateRate];
+            return true;
+        }
+
+        /// <summary>Generates the tone and returns a mix to be added to the output.</summary>
+        protected override float[] Collect() {
+            Array.Clear(rendered, 0, rendered.Length);
+            if (IsPlaying && !Mute && testTone != null) {
+                int delay = channel * testTone.Length, channels = Listener.Channels.Length;
+                int pos = TimeSamples - delay;
+                for (int sample = channel; sample < rendered.Length; sample += channels) {
+                    if (pos < 0) {
+                        ++pos; // TODO: optimize
+                        continue;
+                    }
+                    if (pos >= testTone.Length) {
+                        Array.Clear(rendered, sample, rendered.Length - sample);
+                        IsPlaying = false;
+                        break;
+                    }
+                    rendered[sample] = testTone[pos];
+                    ++pos;
+                }
+                TimeSamples += listener.UpdateRate;
+            }
+            return rendered;
+        }
+    }
+}
--- CavernUnity DLL/QuickEQ/SweepChannel.cs
+++ CavernUnity DLL/QuickEQ/SweepChannel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
+
+using Cavern.QuickEQ.SignalGeneration;
 
 namespace Cavern.QuickEQ {
     /// <summary>Runs the sweep of a <see cref="SpeakerSweeper"/> with a correct delay for the given channel.</summary>
@@ -13,50 +15,12 @@ namespace Cavern.QuickEQ {
         [Tooltip("Sweeper to use the sweep reference of.")]
         public SpeakerSweeper Sweeper;
 
-        /// <summary>Custom Cavern <see cref="Source"/> for this component.</summary>
-        class SweepChannelSource : Source {
-            /// <summary>Target output channel.</summary>
-            public int Channel = 0;
-            /// <summary>Sweeper to use the sweep reference of.</summary>
-            public SpeakerSweeper Sweeper;
-
-            /// <summary>Rendered output array kept to save allocation time.</summary>
-            float[] rendered = new float[0];
-
-            protected override bool Precollect() {
-                if (rendered.Length != Listener.Channels.Length * AudioListener3D.Current.UpdateRate)
-                    rendered = new float[Listener.Channels.Length * AudioListener3D.Current.UpdateRate];
-                return true;
-            }
-
-            protected override float[] Collect() {
-                float[] samples = Sweeper.SweepReference;
-                Array.Clear(rendered, 0, rendered.Length);
-                if (IsPlaying && !Mute && samples != null) {
-                    int sweepLength = samples.Length, delay = Channel * sweepLength, channels = Listener.Channels.Length;
-                    int pos = TimeSamples - delay;
-                    for (int sample = Channel; sample < rendered.Length; sample += channels) {
-                        if (pos < 0)
-                            continue;
-                        if (pos >= sweepLength) {
-                            IsPlaying = false;
-                            break;
-                        }
-                        rendered[sample] = samples[pos];
-                        ++pos;
-                    }
-                    TimeSamples += AudioListener3D.Current.UpdateRate;
-                }
-                return rendered;
-            }
-        }
-
-        void Awake() => cavernSource = new SweepChannelSource();
-
+        [SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Used by Unity lifecycle")]
         void Start() {
-            SweepChannelSource source = (SweepChannelSource)cavernSource;
-            source.Channel = Channel;
-            source.Sweeper = Sweeper;
+            AudioListener3D.cavernListener.DetachSource(cavernSource); // preattached in OnCreate
+            cavernSource = new TimedTestTone(Channel, Sweeper.SweepReference);
+            AudioListener3D.cavernListener.AttachSource(cavernSource);
+            TimedTestTone source = (TimedTestTone)cavernSource;
         }
     }
 }
