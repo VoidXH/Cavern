b5d7f05ad150d6f041fb92d951dd45ac8251d44f
Bence Sgánetz <voidxb@gmail.com>
2021-12-21 20:47:13 +0100

    Equalizer APO Utils basics

---
+++ Cavern.Format/FilterSet/EqualizerAPOUtils.cs
@@ -0,0 +1,32 @@
+﻿using Cavern.Remapping;
+
+namespace Cavern.Format.FilterSet {
+    /// <summary>
+    /// Helper functions for handling Equalizer APO configuration files.
+    /// </summary>
+    public static class EqualizerAPOUtils {
+        internal const string frontLeft = "L";
+        internal const string frontRight = "R";
+        internal const string frontCenter = "C";
+        internal const string screenLFE = "SUB";
+        internal const string rearLeft = "RL";
+        internal const string rearRight = "RR";
+        internal const string sideLeft = "SL";
+        internal const string sideRight = "SR";
+
+        /// <summary>
+        /// Converts Equalizer APO's channel names to <see cref="ReferenceChannel"/> values.
+        /// </summary>
+        public static ReferenceChannel GetReferenceChannel(string apoName) => apoName switch {
+            frontLeft => ReferenceChannel.FrontLeft,
+            frontRight => ReferenceChannel.FrontRight,
+            frontCenter => ReferenceChannel.FrontCenter,
+            screenLFE => ReferenceChannel.ScreenLFE,
+            rearLeft => ReferenceChannel.RearLeft,
+            rearRight => ReferenceChannel.RearRight,
+            sideLeft => ReferenceChannel.SideLeft,
+            sideRight => ReferenceChannel.SideRight,
+            _ => ReferenceChannel.Unused,
+        };
+    }
+}
