c6b4aea0c43d81b5dfa8c033f1686ca39abdc8a0
Bence Sgánetz <voidxb@gmail.com>
2022-01-08 20:14:10 +0100

    Completed Wavefront Simulator

--- CavernSamples/WavefrontSimulator/MainWindow.xaml.cs
+++ CavernSamples/WavefrontSimulator/MainWindow.xaml.cs
@@ -5,13 +5,16 @@ using Cavern.Remapping;
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Drawing;
 using System.Numerics;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+
 using Color = System.Drawing.Color;
 
 namespace WavefrontSimulator {
@@ -21,7 +24,7 @@ namespace WavefrontSimulator {
     public partial class MainWindow : Window {
         struct Simulated {
             Vector2 position;
-            float[] samples;
+            readonly float[] samples;
 
             public Vector2 Position { get; private set; }
             public Vector2 Direction { get; private set; }
@@ -54,13 +57,18 @@ namespace WavefrontSimulator {
         readonly List<Simulated> data = new();
         int sampleRate;
 
-        const float roomSize = 5; // meters, TODO: slider
-        const float directivityIndex = 3; // TODO: slider
-        // TODO: calculate uniformity, export image
+        float wallLength = 5;
+        float directivityIndex = 3;
 
-        public MainWindow() => InitializeComponent();
+        public MainWindow() {
+            InitializeComponent();
+            toeIn.IsChecked = Settings.Default.toeIn;
+            realTime.IsChecked = Settings.Default.realTime;
+            dirIndexValue.Value = Settings.Default.dirIndex;
+            wallLenValue.Value = Settings.Default.wallLen;
+        }
 
-        void LoadImpulseResponse(object sender, RoutedEventArgs e) {
+        void LoadImpulseResponse(object _, RoutedEventArgs e) {
             OpenFileDialog dialog = new() {
                 Filter = "RIFF WAVE files (*.wav)|*.wav"
             };
@@ -72,15 +80,40 @@ namespace WavefrontSimulator {
                 ChannelPrototype channel = ChannelPrototype.FromStandardName(channelName);
                 data.Add(new Simulated(new Channel(channel.X, channel.Y), samples));
                 sampleRate = reader.SampleRate;
+                clear.IsEnabled = true;
             }
         }
 
+        void Clear(object _, RoutedEventArgs e) {
+            data.Clear();
+            clear.IsEnabled = false;
+        }
+
+        void DirectivityIndexChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            directivityIndex = (float)Math.Round(((Slider)sender).Value * 10) * .1f;
+            if (dirIndex != null)
+                dirIndex.Text = directivityIndex.ToString("0.0");
+            if (realTime.IsChecked.Value && data.Count != 0)
+                Render(null, null);
+        }
+
+        void WallLengthChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            wallLength = (float)Math.Round(((Slider)sender).Value * 10) * .1f;
+            if (wallLen != null)
+                wallLen.Text = wallLength.ToString("0.0 m");
+            if (realTime.IsChecked.Value && data.Count != 0)
+                Render(null, null);
+        }
+
         static Color DrawPixel(float strength) {
-            return Color.FromArgb(
-                Math.Max((int)(255 * (1 - Math.Abs(2 * strength))), 0),
-                (int)(127 * 1 - Math.Abs(2 * (strength - .5f))),
-                Math.Max((int)(255 * (1 - Math.Abs(2 * (strength - 1)))), 0)
-            );
+            if (!float.IsNaN(strength))
+                return Color.FromArgb(
+                    Math.Max((int)(255 * (1 - Math.Abs(2 * strength))), 0),
+                    (int)(127 * 1 - Math.Abs(2 * (strength - .5f))),
+                    Math.Max((int)(255 * (1 - Math.Abs(2 * (strength - 1)))), 0)
+                );
+            else
+                return Color.Black;
         }
 
         [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
@@ -97,13 +130,17 @@ namespace WavefrontSimulator {
             }
         }
 
-        void Render(object sender, RoutedEventArgs e) {
-            // TODO: was anything loaded
+        void Render(object _, RoutedEventArgs e) {
+            if (data.Count == 0) {
+                MessageBox.Show("No impulse responses were imported.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
             float[] sine = new float[data[0].SampleCount];
             const float frequency = 1000;
             for (int i = 0; i < sine.Length; ++i)
                 sine[i] = MathF.Sin(2 * MathF.PI * frequency * i / sampleRate);
-            float roomSizeUnit = roomSize / 2;
+            float roomSizeUnit = wallLength / 2;
             for (int i = 0; i < data.Count; ++i)
                 data[i] = data[i].WorkWith(roomSizeUnit, sine);
 
@@ -112,7 +149,7 @@ namespace WavefrontSimulator {
             float maxGain = 0, minGain = float.PositiveInfinity;
             for (int x = 0; x < size; ++x) {
                 for (int y = 0; y < size; ++y) {
-                    Vector2 dotPos = new Vector2(x, y) * new Vector2(roomSize / size) - new Vector2(roomSizeUnit);
+                    Vector2 dotPos = new Vector2(x, y) * new Vector2(wallLength / size) - new Vector2(roomSizeUnit);
                     float result = 0;
                     for (int i = 0; i < data.Count; ++i) {
                         Vector2 distance = dotPos - data[i].Position;
@@ -130,6 +167,17 @@ namespace WavefrontSimulator {
                         minGain = gains[x, y];
                 }
             }
+
+            float uniformityValue = 0, center = (maxGain - minGain) * .5f, middle = minGain + center;
+            for (int x = 0; x < size; ++x) {
+                for (int y = 0; y < size; ++y) {
+                    float addition = MathF.Abs(gains[x, y] - middle);
+                    if (!float.IsNaN(addition))
+                        uniformityValue += addition;
+                }
+            }
+            uniformityValue = 1 - (uniformityValue / (size * size * center));
+            uniformity.Text = "Uniformity: " + uniformityValue.ToString("0.00%");
             
             Bitmap output = new Bitmap(size, size);
             maxGain -= minGain;
@@ -139,7 +187,30 @@ namespace WavefrontSimulator {
                     output.SetPixel(x, size - y - 1, DrawPixel(pxval));
                 }
             }
+            image.Tag = output;
             image.Source = ToImageSource(output);
+        }
+
+        void ExportRender(object sender, RoutedEventArgs e) {
+            if (image.Tag == null) {
+                MessageBox.Show("Please render an image first.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            SaveFileDialog dialog = new() {
+                FileName = $"Waveform simulation - {uniformity.Text.Replace(":", " -").Replace(',', '.')}.bmp",
+                Filter = "Bitmap files (*.bmp)|*.bmp"
+            };
+            if (dialog.ShowDialog() == true)
+                ((Bitmap)image.Tag).Save(dialog.FileName);
+        }
+
+        void OnExit(object _, CancelEventArgs e) {
+            Settings.Default.toeIn = toeIn.IsChecked.Value;
+            Settings.Default.realTime = realTime.IsChecked.Value;
+            Settings.Default.dirIndex = (float)dirIndexValue.Value;
+            Settings.Default.wallLen = (float)wallLenValue.Value;
+            Settings.Default.Save();
         }
     }
 }
