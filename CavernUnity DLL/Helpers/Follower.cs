a72606c2a6ab94869fd759dc38d284457e0c8ccb
Bence Sgánetz <voidxb@gmail.com>
2022-04-09 02:43:26 +0200

    Ability to mute displayed objects

--- CavernUnity DLL/Helpers/Follower.cs
+++ CavernUnity DLL/Helpers/Follower.cs
@@ -18,17 +18,31 @@ namespace Cavern.Helpers {
         /// <summary>
         /// Attach the visualized source to the active listener.
         /// </summary>
+        [Tooltip("Attach the visualized source to the active listener.")]
         public bool attach;
+
+        /// <summary>
+        /// Mute the source of the displayed object.
+        /// </summary>
+        [Tooltip("Mute the source of the displayed object.")]
+        public bool mute;
+
+        /// <summary>
+        /// Access to material properties.
+        /// </summary>
+        new Renderer renderer;
 
         /// <summary>
         /// Create a follower for a target source.
         /// </summary>
-        public static GameObject CreateFollower(Source target, bool attach) {
+        /// <returns>The follower component on a created sphere</returns>
+        public static Follower CreateFollower(Source target, bool attach) {
             GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
             Follower follower = obj.AddComponent<Follower>();
             follower.target = target;
             follower.attach = attach;
-            return obj;
+            follower.renderer = obj.GetComponent<Renderer>();
+            return follower;
         }
 
         [SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Used by Unity lifecycle")]
@@ -50,6 +64,10 @@ namespace Cavern.Helpers {
             if (target != null) {
                 Transform listenerTransform = AudioListener3D.Current.transform;
                 transform.position = listenerTransform.position + listenerTransform.rotation * target.Position.VectorMatch();
+                target.Mute = mute;
+                Color newColor = mute ? Color.red : Color.white;
+                if (renderer.material.color != newColor)
+                renderer.material.color = newColor;
             }
         }
     }
