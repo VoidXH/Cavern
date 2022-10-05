e0650333c7da16ee11c7ee12676b63d0a1b7f60a
Bence Sgánetz <voidxb@gmail.com>
2022-10-05 22:41:57 +0200

    Cleanup script for external archival

---
+++ Cleanup.bat
@@ -0,0 +1,25 @@
+rd /s /q .vs
+rd /s /q Cavern\bin
+rd /s /q Cavern\obj
+rd /s /q Cavern.Format\bin
+rd /s /q Cavern.Format\obj
+rd /s /q Cavern.QuickEQ\bin
+rd /s /q Cavern.QuickEQ\obj
+rd /s /q "CavernUnity DLL\bin"
+rd /s /q "CavernUnity DLL\obj"
+rd /s /q CavernSamples\.vs
+rd /s /q CavernSamples\Benchmark\bin
+rd /s /q CavernSamples\Benchmark\obj
+rd /s /q CavernSamples\CavernizeGUI\bin
+rd /s /q CavernSamples\CavernizeGUI\obj
+rd /s /q CavernSamples\Deconvolver\bin
+rd /s /q CavernSamples\Deconvolver\obj
+rd /s /q CavernSamples\HRTFSetImporter\bin
+rd /s /q CavernSamples\HRTFSetImporter\obj
+rd /s /q CavernSamples\HRTFSetStatista\bin
+rd /s /q CavernSamples\HRTFSetStatista\obj
+rd /s /q CavernSamples\WAVChannelReorderer\bin
+rd /s /q CavernSamples\WAVChannelReorderer\obj
+rd /s /q CavernSamples\WavefrontSimulator\bin
+rd /s /q CavernSamples\WavefrontSimulator\obj
+pause
