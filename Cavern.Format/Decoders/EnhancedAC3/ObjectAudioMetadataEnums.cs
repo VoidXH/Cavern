c3ea4902401953db9974e17a3b01ed8a42110281
Bence Sgánetz <voidxb@gmail.com>
2022-10-17 20:59:14 +0200

    Support channel-based DD+ JOC streams

--- Cavern.Format/Decoders/EnhancedAC3/ObjectAudioMetadataEnums.cs
+++ Cavern.Format/Decoders/EnhancedAC3/ObjectAudioMetadataEnums.cs
@@ -1,23 +1,23 @@
 ﻿namespace Cavern.Format.Decoders.EnhancedAC3 {
     enum NonStandardBedChannel {
-        Max = 17,
-        FrontLeft = 16,
-        FrontRight = 15,
-        Center = 14,
-        LowFrequencyEffects = 13,
-        SurroundLeft = 12,
-        SurroundRight = 11,
-        RearLeft = 10,
-        RearRight = 9,
+        FrontLeft = 0,
+        FrontRight = 1,
+        Center = 2,
+        LowFrequencyEffects = 3,
+        SurroundLeft = 4,
+        SurroundRight = 5,
+        RearLeft = 6,
+        RearRight = 7,
         TopFrontLeft = 8,
-        TopFrontRight = 7,
-        TopSurroundLeft = 6,
-        TopSurroundRight = 5,
-        TopRearLeft = 4,
-        TopRearRight = 3,
-        WideLeft = 2,
-        WideRight = 1,
-        LowFrequencyEffects2 = 0
+        TopFrontRight = 9,
+        TopSurroundLeft = 10,
+        TopSurroundRight = 11,
+        TopRearLeft = 12,
+        TopRearRight = 13,
+        WideLeft = 14,
+        WideRight = 15,
+        LowFrequencyEffects2 = 16,
+        Max = 17
     }
 
     enum ObjectAnchor {
