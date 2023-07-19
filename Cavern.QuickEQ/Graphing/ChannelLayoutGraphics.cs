0a416fc29def4b7d74d5bac5297bb15626e54d90
Bence Sgánetz <voidxb@gmail.com>
2023-07-19 15:05:57 +0200

    Channel layout graphics creator

---
+++ Cavern.QuickEQ/Graphing/ChannelLayoutGraphics.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace Cavern.QuickEQ.Graphing {
+    /// <summary>
+    /// Draws a channel layout as an overhead image.
+    /// </summary>
+    public class ChannelLayoutGraphics {
+        /// <summary>
+        /// Width of the resulting image in pixels.
+        /// </summary>
+        public int Width { get; set; } = 512;
+
+        /// <summary>
+        /// Height of the resulting image in pixels.
+        /// </summary>
+        public int Height { get; set; } = 512;
+
+        /// <summary>
+        /// Side length of a square marking a channel.
+        /// </summary>
+        public int ChannelWidth { get; set; } = 64;
+
+        /// <summary>
+        /// Width of the connection lines for speaker layers.
+        /// </summary>
+        public int ConnectionWidth { get; set; } = 4;
+
+        /// <summary>
+        /// ARGB color of the drawn channels.
+        /// </summary>
+        public uint ChannelColor { get; set; } = 0xFFFFFFFF;
+
+        /// <summary>
+        /// ARGB color of the connections of channels on the same height plane.
+        /// </summary>
+        public uint ConnectionColor { get; set; } = 0x7FFFFFFF;
+
+        /// <summary>
+        /// ARGB color of the pixels not used for drawing the layout.
+        /// </summary>
+        public uint BackgroundColor { get; set; } = 0;
+
+        /// <summary>
+        /// Draws a channel layout as an overhead image. The pixels are in ARGB.
+        /// </summary>
+        public uint[] Draw(Channel[] channels) {
+            uint[] image = new uint[Width * Height];
+            for (int i = 0; i < image.Length; i++) {
+                image[i] = BackgroundColor;
+            }
+
+            float lastElevation = float.NaN; // This optimization is enough, as channels are usually grouped by elevation
+            for (int i = 0; i < channels.Length; i++) {
+                if (channels[i].LFE) {
+                    continue; // LFE is drawn to the center
+                }
+                float elevation = channels[i].X;
+                if (lastElevation != elevation) {
+                    DrawConnection(image, elevation);
+                    lastElevation = elevation;
+                }
+            }
+
+            int workingWidth = Width - ChannelWidth;
+            int workingHeight = Height - ChannelWidth;
+            for (int i = 0; i < channels.Length; i++) {
+                float xPadding, yPadding;
+                if (channels[i].LFE) {
+                    xPadding = .5f;
+                    yPadding = .5f;
+                } else {
+                    float relativePadding = .5f - MathF.Abs(channels[i].X) * (1f / (90 * 2));
+                    xPadding = channels[i].CubicalPos.X * relativePadding + .5f;
+                    yPadding = channels[i].CubicalPos.Z * relativePadding + .5f;
+                }
+                Fill(image, ChannelColor, (int)(workingWidth * xPadding), (int)(workingHeight * yPadding), ChannelWidth, ChannelWidth);
+            }
+
+            return image;
+        }
+
+        /// <summary>
+        /// Draws a rectangle which hosts all the channels on that elevation.
+        /// </summary>
+        void DrawConnection(uint[] image, float elevation) {
+            float relativePadding = MathF.Abs(elevation) * (1f / (90 * 2));
+            int extraPadding = (ChannelWidth >> 1) - (ConnectionWidth >> 1),
+                hPadding = (int)((Width - (extraPadding << 1)) * relativePadding) + extraPadding,
+                vPadding = (int)((Height - (extraPadding << 1)) * relativePadding) + extraPadding;
+            Fill(image, ConnectionColor, hPadding, vPadding, ConnectionWidth, Height - (vPadding << 1)); // Left
+            Fill(image, ConnectionColor, Width - hPadding - ConnectionWidth, vPadding, ConnectionWidth, Height - (vPadding << 1)); // Right
+            Fill(image, ConnectionColor, hPadding, vPadding, Width - (hPadding << 1), ConnectionWidth); // Top
+            Fill(image, ConnectionColor, hPadding, Height - vPadding - ConnectionWidth, Width - (hPadding << 1), ConnectionWidth); // Bottom
+        }
+
+        /// <summary>
+        /// Draw a colored rectangle on the image in production.
+        /// </summary>
+        void Fill(uint[] image, uint color, int x, int y, int width, int height) {
+            int pos = y * Width + x;
+            for (int yAdd = 0; yAdd < height; yAdd++) {
+                for (int xAdd = 0; xAdd < width; xAdd++) {
+                    image[pos + xAdd] = color;
+                }
+                pos += Width;
+            }
+        }
+    }
+}
--- CavernUnity DLL/QuickEQ/Graphing/GraphRenderExtensions.cs
+++ CavernUnity DLL/QuickEQ/Graphing/GraphRenderExtensions.cs
@@ -8,19 +8,31 @@ namespace Cavern.QuickEQ.Graphing {
         /// <summary>
         /// Copies the rendered graph to a Unity texture.
         /// </summary>
-        public static Texture2D ConvertToTexture(this GraphRenderer renderer) {
-            Texture2D texture = new Texture2D(renderer.Width, renderer.Height);
-            ConvertToTexture(renderer, texture, new Color[renderer.Pixels.Length]);
+        public static Texture2D ConvertToTexture(this GraphRenderer renderer) =>
+            renderer.Pixels.ConvertToTexture(renderer.Width, renderer.Height);
+
+        /// <summary>
+        /// Copies a rendered image to a Unity texture.
+        /// </summary>
+        public static Texture2D ConvertToTexture(this uint[] image, int width, int height) {
+            Texture2D texture = new Texture2D(width, height);
+            ConvertToTexture(image, texture, new Color[image.Length]);
             return texture;
         }
 
         /// <summary>
         /// Copies the rendered graph to an existing Unity texture and color cache the size of <paramref name="reusedTexture"/>'s Pixels.
         /// </summary>
-        public static unsafe void ConvertToTexture(this GraphRenderer renderer, Texture2D reusedTexture, Color[] reusedColors) {
-            fixed (uint* pData = renderer.Pixels)
+        public static void ConvertToTexture(this GraphRenderer renderer, Texture2D reusedTexture, Color[] reusedColors) =>
+            ConvertToTexture(renderer.Pixels, reusedTexture, reusedColors);
+
+        /// <summary>
+        /// Copies a rendered image to an existing Unity texture and color cache the size of <paramref name="reusedTexture"/>'s Pixels.
+        /// </summary>
+        public static unsafe void ConvertToTexture(this uint[] source, Texture2D reusedTexture, Color[] reusedColors) {
+            fixed (uint* pData = source)
             fixed (Color* pColors = reusedColors) {
-                uint* data = pData, end = data + renderer.Pixels.Length;
+                uint* data = pData, end = data + source.Length;
                 Color* color = pColors;
                 while (data != end) {
                     *color++ = new Color32((byte)((*data >> 16) & 0xFF), (byte)((*data >> 8) & 0xFF), (byte)(*data & 0xFF),
