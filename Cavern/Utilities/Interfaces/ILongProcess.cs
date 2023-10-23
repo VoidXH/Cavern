b5fe6b5e4ac306677368a9988aa5d3afcee91a67
Bence Sgánetz <voidxb@gmail.com>
2023-10-23 15:18:44 +0200

    Long process interface

---
+++ Cavern/Utilities/Interfaces/ILongProcess.cs
@@ -0,0 +1,11 @@
+﻿namespace Cavern.Utilities {
+    /// <summary>
+    /// An operation that takes a long time and should run in the background. Its interface provides progress polling.
+    /// </summary>
+    public interface ILongProcess {
+        /// <summary>
+        /// The ratio of doneness [0;1].
+        /// </summary>
+        float Progress { get; }
+    }
+}
