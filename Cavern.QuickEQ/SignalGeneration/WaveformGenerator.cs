e3b9ff8a723263dc6cd5b044cce794cb1e967bad
VoidX <voidxb@gmail.com>
2024-11-27 17:28:10 +0100

    Sample rate-based sine wave generation

--- Cavern.QuickEQ/SignalGeneration/WaveformGenerator.cs
+++ Cavern.QuickEQ/SignalGeneration/WaveformGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 using Cavern.Filters;
 using Cavern.QuickEQ.Equalization;
@@ -12,13 +13,24 @@ namespace Cavern.QuickEQ.SignalGeneration {
         /// <summary>
         /// Generates a sine wave signal.
         /// </summary>
-        /// <param name="frequency">The frequency of the sine wave in Hertz</param>
+        /// <param name="frequency">The frequency of the sine wave in periods/<paramref name="length"/></param>
         /// <param name="length">The length of the generated signal in samples</param>
         /// <remarks>No windowing is used, and a click will be heard if the last period doesn't end at
         /// the <paramref name="length"/>.</remarks>
-        public static float[] Sine(float frequency, int length) {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float[] Sine(float frequency, int length) => Sine(frequency, length, length);
+
+        /// <summary>
+        /// Generates a sine wave signal.
+        /// </summary>
+        /// <param name="frequency">The frequency of the sine wave in Hertz</param>
+        /// <param name="length">The length of the generated signal in samples</param>
+        /// <param name="sampleRate">Samples per second</param>
+        /// <remarks>No windowing is used, and a click will be heard if the last period doesn't end at
+        /// the <paramref name="length"/>.</remarks>
+        public static float[] Sine(float frequency, int length, int sampleRate) {
             float[] result = new float[length];
-            float mul = 2 * MathF.PI * frequency / length;
+            float mul = 2 * MathF.PI * frequency / sampleRate;
             for (int i = 0; i < length; i++) {
                 result[i] = MathF.Sin(mul * i);
             }
