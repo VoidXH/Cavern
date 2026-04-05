03e4d5c0bc8ec983ec79648d45700da48ad2b5e3
VoidX <voidxb@gmail.com>
2026-04-05 02:58:55 +0200

    New string extensions

--- Cavern.QuickEQ.Format/Utilities/StringExtensions.cs
+++ Cavern.QuickEQ.Format/Utilities/StringExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace Cavern.Format.Utilities {
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Cavern.Format.Utilities {
     /// <summary>
     /// String operations not supported by .NET.
     /// </summary>
@@ -14,6 +17,34 @@
                 }
             }
             return str;
+        }
+
+        /// <summary>
+        /// Get if the string is made of the same character, and if so, which one. Returns 0 if the string is empty or contains different characters.
+        /// </summary>
+        public static char IsTheSameCharacter(this string str) {
+            if (str.Length == 0) {
+                return '\0';
+            }
+
+            char start = str[0];
+            for (int i = 1; i < str.Length; i++) {
+                if (str[i] != start) {
+                    return '\0';
+                }
+            }
+            return start;
+        }
+
+        /// <summary>
+        /// Reads a string line by line with very little memory overhead.
+        /// </summary>
+        public static IEnumerable<string> ReadLines(this string source) {
+            using StringReader reader = new StringReader(source);
+            string line;
+            while ((line = reader.ReadLine()) != null) {
+                yield return line;
+            }
         }
 
         /// <summary>
