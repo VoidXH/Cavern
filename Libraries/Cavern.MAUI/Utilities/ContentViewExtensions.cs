2610409eb77165462a3915a0bafe719f02004d74
VoidX <voidxb@gmail.com>
2026-04-17 23:27:57 +0200

    Ability to show alerts from user controls in MAUI

---
+++ Libraries/Cavern.MAUI/Utilities/ContentViewExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Cavern.MAUI.Utilities;
+
+/// <summary>
+/// Functions that should have been present in <see cref="ContentView"/>s.
+/// </summary>
+public static class ContentViewExtensions {
+    /// <summary>
+    /// Show an alert dialog with a single <paramref name="cancel"/> button (of which the text is its argument).
+    /// </summary>
+    public static Task DisplayAlertAsync(this ContentView view, string title, string message, string cancel) {
+        if (view.Parent is not Page page) {
+            return Task.CompletedTask;
+        }
+        return page.DisplayAlertAsync(title, message, cancel);
+    }
+}
