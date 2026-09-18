7f93ebe7736fada9087eaaee95b168354fe18ea4
VoidX <voidxb@gmail.com>
2026-09-18 02:52:05 +0200

    Base64 serialization interface

---
+++ Cavern/Utilities/Interfaces/IBase64Serializable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Cavern.Utilities {
+    /// <summary>
+    /// An object that can be serialized to and deserialized from a base64 string.
+    /// </summary>
+    public interface IBase64Serializable {
+        /// <summary>
+        /// Serialize this object to a base64 string.
+        /// </summary>
+        string ToBase64();
+
+        /// <summary>
+        /// Deserialize this object from a base64 string.
+        /// </summary>
+        void FromBase64(string source);
+    }
+}
